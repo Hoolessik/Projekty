292ad63503a017d2cb0f377487432f3981e9ad31
Hoolessik <krexongames@gmail.com>
2023-04-21 17:17:37 +0200

    strona cdn

--- strona/src/components/Navbar.js
+++ strona/src/components/Navbar.js
@@ -7,20 +7,37 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <>
+      <>
         <nav className='navbar'>
             <div className='navbar-container'>
                 <Link to='/' className='navbar-logo'>
-                  <img src={logo}></img>
+                  <img src={logo}/>
                 </Link>
                 <div className='menu-icon' OnClick={handleClick}>
-                  <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                  <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                  <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                      Home
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                      Services
+                    </Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                      Products
+                    </Link>
+                  </li>
+                </ul>
             </div>
-        </nav>
-    
-    </>
+        </nav>   
+      </>
   )
 }
 
