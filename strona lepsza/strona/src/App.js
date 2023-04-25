5ebb9c8ed4ec3f0e7966ca947c322965eaa90921
Hoolessik <krexongames@gmail.com>
2023-04-25 16:54:28 +0200

    lepsza strona

---
+++ strona lepsza/strona/src/App.js
@@ -0,0 +1,12 @@
+import Banner from './Banner'
+import Navbar from './Navbar'
+import Cards from './Cards'
+export default function App () {
+  return <>
+  <Navbar/>
+  <Banner/>
+  <Banner/>
+  <Cards/>
+  </>  
+
+}
